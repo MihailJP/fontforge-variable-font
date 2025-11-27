2f3f968a06130fadd5eb62c29dd949e0e38b3734
MihailJP <mihailjp@gmail.com>
2025-11-27 23:40:47 +0900

    Mockup

--- fontforge_variable_font.py
+++ fontforge_variable_font.py
@@ -1,17 +1,63 @@
 import fontforge
 
-def mockup(u, glyph):
+def loadMenu(u, glyph):
     fontforge.postNotice("This is a mockup", "Not yet implemented!")
 
 
-def mockupEnable(u, glyph):
-    return True
+def loadEnable(u, glyph):
+    return False
+
+
+def saveMenu(u, glyph):
+    fontforge.postNotice("This is a mockup", "Not yet implemented!")
+
+
+def saveEnable(u, glyph):
+    return False
+
+
+def designAxesMenu(u, glyph):
+    fontforge.postNotice("This is a mockup", "Not yet implemented!")
+
+
+def designAxesEnable(u, glyph):
+    return False
+
+
+def instanceMenu(u, glyph):
+    fontforge.postNotice("This is a mockup", "Not yet implemented!")
+
+
+def instanceEnable(u, glyph):
+    return False
 
 
 def fontforge_plugin_init(**kw):
     fontforge.registerMenuItem(
-        callback=mockup,
-        enable=mockupEnable,
+        callback=loadMenu,
+        enable=loadEnable,
         context="Font",
-        name="_Variable Font"
+        submenu="_Variable Font",
+        name="_Open a variable font..."
+    )
+    fontforge.registerMenuItem(
+        callback=saveMenu,
+        enable=saveEnable,
+        context="Font",
+        submenu="_Variable Font",
+        name="_Generate a variable font..."
+    )
+    fontforge.registerMenuItem(
+        callback=designAxesMenu,
+        enable=designAxesEnable,
+        context="Font",
+        submenu="_Variable Font",
+        name="Design _axes..."
+    )
+    fontforge.registerMenuItem(
+        callback=instanceMenu,
+        enable=instanceEnable,
+        context="Font",
+        submenu="_Variable Font",
+        name="_Instance list..."
     )
